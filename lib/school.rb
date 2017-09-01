947a7bfe12e3c878272709e07729107effa9e8dd
github username <github email address>
2017-09-01 17:37:33 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if !@roster[grade]
+      @roster[grade] = []
+    end
+      @roster[grade] << student
+  end
+
+  def grade(grade)
+    #loop through and puts out each array value
+    grade_array = []
+    @roster.each do |grades, student|
+      if grades == grade
+        student.each do |student_name|
+          grade_array << student_name
+        end
+      end
+    end
+    grade_array
+  end
+
+  def sort
+    @roster.map { |grade, student| [grade, student.sort]}.to_h
+
+  end
+
+
+end
